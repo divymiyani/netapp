46a2da4a3e20128dee0b566882e7216687d4ab2e
divymiyani <divymiyani007@gmail.com>
2023-07-17 16:10:06 -0700

    loadedpages add

--- lib/image_store.dart
+++ lib/image_store.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:netapp/sample_post_image.dart';
 import 'package:http/http.dart' as http;
@@ -10,7 +11,6 @@ class ImageStore = _ImageStore with _$ImageStore;
 abstract class _ImageStore with Store {
   int currentPage = 1;
   int itemsPerPage = 10;
-  int totalPages = 10;
   int loadedPages = 0;
 
   @observable
@@ -24,28 +24,85 @@ abstract class _ImageStore with Store {
 
   @action
   Future<void> getData() async {
-    if (currentPage <= totalPages) {
-      final response = await http.get(Uri.parse(
-          'https://jsonplaceholder.typicode.com/users?_page=$currentPage&_limit=$itemsPerPage'));
+    final response = await http.get(Uri.parse(
+        'https://jsonplaceholder.typicode.com/users?_page=$currentPage&_limit=$itemsPerPage'));
 
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        final newPostsImages = List<SamplePostImages>.from(
-            data.map((x) => SamplePostImages.fromJson(x)));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final newPostsImages = List<SamplePostImages>.from(
+          data.map((x) => SamplePostImages.fromJson(x)));
 
-        setSamplePostsImages(newPostsImages);
-      }
-
-      loadedPages++;
-      if (loadedPages >= totalPages) {
-        currentPage = 1;
-        loadedPages = 0;
-      }
+      setSamplePostsImages(newPostsImages);
     }
+
+    loadedPages++;
   }
 
   @action
   void loadMoreData() {
-    getData();
+    if (loadedPages < 10) {
+      getData();
+    }
   }
 }
+
+
+
+
+
+
+
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:mobx/mobx.dart';
+// import 'package:netapp/sample_post_image.dart';
+// import 'package:http/http.dart' as http;
+
+// part 'image_store.g.dart';
+
+// class ImageStore = _ImageStore with _$ImageStore;
+
+// abstract class _ImageStore with Store {
+//   int currentPage = 1;
+//   int itemsPerPage = 10;
+//   int totalPages = 10;
+//   int loadedPages = 0;
+
+//   @observable
+//   ObservableList<SamplePostImages> samplePostsImages =
+//       ObservableList<SamplePostImages>.of([]);
+
+//   @action
+//   void setSamplePostsImages(List<SamplePostImages> data) {
+//     samplePostsImages.addAll(data);
+//   }
+
+//   @action
+//   Future<void> getData() async {
+//     if (currentPage <= totalPages) {
+//       final response = await http.get(Uri.parse(
+//           'https://jsonplaceholder.typicode.com/users?_page=$currentPage&_limit=$itemsPerPage'));
+
+//       if (response.statusCode == 200) {
+//         final data = jsonDecode(response.body);
+//         final newPostsImages = List<SamplePostImages>.from(
+//             data.map((x) => SamplePostImages.fromJson(x)));
+
+//         setSamplePostsImages(newPostsImages);
+//       }
+
+//       loadedPages++;
+//       if (loadedPages == totalPages) {
+//         currentPage = 1;
+//         loadedPages = 0;
+//       } else {
+//         Text('no more data!');
+//       }
+//     }
+//   }
+
+//   @action
+//   void loadMoreData() {
+//     getData();
+//   }
+// }
